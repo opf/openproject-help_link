d0c744d934afd2c53e85300dc2217c6df728b9ee
Felix Schäfer <felix@fachschaften.org>
2011-06-24 18:07:59 +0200

    Correct the step definition to the new plugin name.

--- features/step_definitions/help_relinking.rb
+++ features/step_definitions/help_relinking.rb
@@ -1,9 +1,9 @@
 Given /^the help link setting points to the project (.+)$/ do |project_name|
   project_name.gsub!("\"","")
   p = Project.find_by_name(project_name)
-  new_values = Setting.plugin_redmine_pi_customizing
+  new_values = Setting.plugin_chiliproject_help_link
   new_values["help_link_target"] = "/projects/#{p.identifier}"
-  Setting.plugin_redmine_pi_customizing = new_values
+  Setting.plugin_chiliproject_help_link = new_values
 
   Redmine::MenuManager.map :top_menu do |menu|
     menu.delete :help
