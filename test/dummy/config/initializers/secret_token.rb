2bf20acd19ab76215f9739d1d0124651f9f6d8d5
Hagen Schink <h.schink@finn.de>
2013-03-05 16:58:32 +0100

    Removed unused tests

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'eb0ce4bc6b23de8e64ba60e3bdbf37bcff02417fcaf149ade1d32629b91ec84a819e669346d860a82e3832b0e3903069703d2dcbd3550d357639746b344650b0'
--- test/openproject_help_link_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class HelpLinkTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, HelpLink
-  end
-end
