f087f0d5e1c4da5d8ea97aa9ecdfca044ee85b7e
Sergiu Rosca <rmagnum2002@gmail.com>
2015-03-11 18:13:59 +0200

    changed cucumber steps to point to help link instead of projects/support

--- features/step_definitions/help_relinking.rb
+++ features/step_definitions/help_relinking.rb
@@ -17,15 +17,21 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #++
 
-Given /^the help link setting points to the project (.+)$/ do |project_name|
-  project_name.gsub!("\"", '')
-  p = Project.find_by_name(project_name)
+Given(/^the help link setting points to help page$/) do
   new_values = Setting.plugin_openproject_help_link
-  new_values['help_link_target'] = "/projects/#{p.identifier}"
+  new_values["help_link_target"] = OpenProject::Info.help_url
   Setting.plugin_openproject_help_link = new_values
 
   Redmine::MenuManager.map :top_menu do |menu|
     menu.delete :help
-    menu.push :help, OpenProject::Info.help_url, last: true
+    menu.push :help, OpenProject::Info.help_url, :last => true
   end
+end
+
+When(/^I visit the root page$/) do
+  visit '/'
+end
+
+Then(/^I should have the help link$/) do
+  page.should have_link('Help', :href => OpenProject::Info.help_url)
 end
